d337bb39d46bd25e4f459715a75898d2a85f5bb0
raymo <raymo@DESKTOP-VIK2O2O>
2021-04-04 22:18:27 +0800

    Exercise 2.2

--- Exercise2/src/WashingMachines.java
+++ Exercise2/src/WashingMachines.java
@@ -11,5 +11,28 @@ public class WashingMachines {
 	System.out.println("The washing machine is on!");
 
 	}
+	
+	void printWashingMachines(String b, String c, double w, int wl) {
+		System.out.println("Brand : " + b);
+		System.out.println("Colour : " + c);
+		System.out.println("Weight : " + w +"kg");
+		System.out.println("Water Level : " + wl + "liter");
+	}
+	
+	void printTime(int t1, int t2, int t3, int t4) {
+		System.out.println("timer:");
+		System.out.println(t1 + "Minutes");
+		System.out.println(t2 + "Minutes");
+		System.out.println(t3 + "Minutes");
+		System.out.println(t4 + "Minutes");
+	}
+	
+	void printMode(String m1, String m2 , String m3) {
+		System.out.println("mode1:" +  m1);
+		System.out.println("mode2:" +  m2);
+		System.out.println("mode3:" +  m3);
+	}
+	
 }
 
+ 
--- Exercise2/src/main.java
+++ Exercise2/src/main.java
@@ -2,30 +2,41 @@ public class main {
 
 	public static void main(String[] args) {
 		WashingMachines a = new WashingMachines();
-		a.brand = "Toshiba";
-		a.colour = "Black";
-		a.weight = 41;
-		a.waterLevel = 80;
-		System.out.println("Washing Machine 1: ");
-		System.out.println(a.brand);
-		System.out.println(a.colour);
-		System.out.println(a.weight + " kg");
-		System.out.println(a.waterLevel + "Liter");
+		//a.brand = "Toshiba";
+		//a.colour = "Black";
+		//a.weight = 41;
+		//a.waterLevel = 80;
+		System.out.println("Washing Machine 1 ");
+		//System.out.println(a.brand);
+		//System.out.println(a.colour);
+		//System.out.println(a.weight + " kg");
+		//System.out.println(a.waterLevel + "Liter");
+		a.printWashingMachines("Toshiba", "Black" , 41 , 80);
 		a.onOff();
+		System.out.println();
+		a.printTime(30, 50, 90, 100);
+		System.out.println();
+		a.printMode("Spin", "Quick", "Regular");
 		
+		System.out.println();
 		System.out.println();
 		
 		WashingMachines b = new WashingMachines();
-		b.brand = "LG";
-		b.colour = "White";
-		b.weight = 50;
-		b.waterLevel = 100;
-		System.out.println("Washing Machine 2: ");
-		System.out.println(b.brand);
-		System.out.println(b.colour);
-		System.out.println(b.weight + " kg");
-		System.out.println(b.waterLevel + "Liter");
+		//b.brand = "LG";
+		//b.colour = "White";
+		//b.weight = 50;
+		//b.waterLevel = 100;
+		System.out.println("Washing Machine 2 ");
+		//System.out.println(b.brand);
+		//System.out.println(b.colour);
+		//System.out.println(b.weight + " kg");
+		//System.out.println(b.waterLevel + "Liter");
+		b.printWashingMachines("LG", "White" , 50 , 100);
 		b.onOff();
+		System.out.println();
+		b.printTime(25, 45 , 60, 90);
+		System.out.println();
+		a.printMode("Heavy Cycle", "Fast", "Synthetics Cycle");
 
 	}
 
